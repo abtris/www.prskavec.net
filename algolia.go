8d79f1c1ff321f04169d4a9efacab893f3baa97a
Ladislav Prskavec <ladislav@prskavec.net>
2020-10-23 09:40:52 +0200

    feat: add algolia code to upload index

---
+++ algolia.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+
+	search "github.com/algolia/algoliasearch-client-go/v3/algolia/search"
+)
+
+type Index struct {
+	Authors      string `json:"authors"`
+	Categories   string `json:"categories"`
+	Content      string `json:"content"`
+	Date         string `json:"date"`
+	Publishdate  string `json:"publishdate"`
+	Kind         string `json:"kind"`
+	Lang         string `json:"lang"`
+	ObjectID     string `json:"objectID"`
+	Summary      string `json:"summary"`
+	Tags         string `json:"tags"`
+	Title        string `json:"title"`
+	Section      string `json:"section"`
+	Type         string `json:"type"`
+	Permalink    string `json:"permalink"`
+	Relpermalink string `json:"relpermalink"`
+}
+
+func main() {
+	algoliaAppID := os.Getenv("ALGOLIA_APP_ID")
+	algoliaAPIKey := os.Getenv("ALGOLIA_API_KEY")
+	algoliaIndexName := os.Getenv("ALGOLIA_INDEX_NAME")
+
+	client := search.NewClient(algoliaAppID, algoliaAPIKey)
+	index := client.InitIndex(algoliaIndexName)
+
+	var records []Index
+	data, _ := ioutil.ReadFile("public/index.json")
+	_ = json.Unmarshal(data, &records)
+  // MAYBE(abtris): filter by type (get only type=post,talk)?
+	_, _ = index.SaveObjects(records)
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/abtris/www.prskavec.net
+
+go 1.15
+
+require github.com/algolia/algoliasearch-client-go/v3 v3.13.0
